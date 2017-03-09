45a769daac103c3a4d11d0f1e9ac63f59cba7f23
zhang-xiao-ming <website_net@163.com>
2017-03-09 18:26:51 +0800

    异常白名单功能优化

--- alarmcenter-facade-impl/src/main/java/com/ymatou/alarmcenter/facade/rest/SystemResourceImpl.java
+++ alarmcenter-facade-impl/src/main/java/com/ymatou/alarmcenter/facade/rest/SystemResourceImpl.java
@@ -20,7 +20,7 @@ public class SystemResourceImpl implements SystemResource {
     @Path("/version")
     @Override
     public String version() {
-        return "2017-01-13.1";
+        return "2017-03-09.1";
     }
 
 
